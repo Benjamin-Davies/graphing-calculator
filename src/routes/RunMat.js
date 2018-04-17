1820979f6063d08938f3d1b182edb9a24231fd8e
Benjamin Davies <bentendavies@gmail.com>
2018-04-17 13:53:58 +1200

    Add mockup calculations to RunMat

--- src/routes/RunMat.js
+++ src/routes/RunMat.js
@@ -6,7 +6,13 @@ import Keyboard from '../components/Keyboard';
 export default () => {
   return (
     <main className="KeyboardRoute">
-      <section className="ExpandedSection">Hello</section>
+      <section className="ExpandedSection container flow-text">
+        <p className="left-align">
+          100-5<sup>2</sup>
+        </p>
+        <p className="right-align">75</p>
+        <p className="left-align">Ans&divide;3</p>
+      </section>
       <Keyboard />
     </main>
   );
